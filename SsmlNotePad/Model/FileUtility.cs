eb7873c518406c1b3469d748544d9975a58b5941
lerwine <lenny@erwinefamily.net>
2016-10-15 23:46:46 -0400

    Add app settings code.

---
+++ SsmlNotePad/Model/FileUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erwine.Leonard.T.SsmlNotePad.Model
+{
+    public static class FileUtility
+    {
+        internal static string ResolveFileUri(string relativeXmlBaseURI)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string EnsureValidExtension(string ssmlFileExtension)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static string ResolveFileUri(string defaultValue_SsmlSchemaCoreFileName, string baseUriPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool GetLocalPath(string path1, out string path2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
